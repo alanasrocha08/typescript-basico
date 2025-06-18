d7a90baf547bed9dc7d82ba612053d41c2937b78
alanasrocha08 <alanasr02@gmail.com>
2025-06-18 11:37:21 -0300

    Adiciona enunciado de exercício

---
+++ exercicio02.ts
@@ -0,0 +1,5 @@
+/* 1) Crie uma inteface para representar um produto com as propriedades nome, preço e garantia. */
+
+/* 2) Cria uma função que receba um objeto baseado na interface Produto e que mostre, preço e garantia. Obs: o objeto deve ser desestruturado como parâmetros individais. */
+
+/* 3) Use a função pelo menos 2x passando produtos diferentes. */
