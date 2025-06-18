60afc49f13f5f9eb375eb859477e67abf823e55d
alanasrocha08 <alanasr02@gmail.com>
2025-06-18 08:54:15 -0300

    Delete modulos.js

--- modulos.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.separador = separador;
-function separador() {
-    console.log("\n-------------------------\n");
-}
